866fe148d4cb72c6f1cc069da64e7c4b3bde0ddf
number <1468288238@qq.com>
2023-10-31 19:32:56 +0800

    first c++ file

---
+++ grade.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    char a;
+    cin >> a;
+    if (a == 'A') cout << "一个学期的努力给你带来了应有的收获" << endl;
+    else if (a == 'B') cout << "你与成功还缺少了一点契机" << endl;
+    else if (a == 'C')cout << "保底已到，力争上游" << endl;
+    else if (a == 'D') cout << "失败与你只是一步之遥，莫一失足成千古恨" << endl;
+    else if (a == 'E') cout << "一次失败代表不了什么，请从头再来" << endl;
+
+    return 0;
+}
